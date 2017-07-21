7d3db83ed5f50eb9cc062f431e6032b189539893
Zachary Becker <zrbecker@gmail.com>
2017-07-21 16:21:01 -0700

    helloflask python project

---
+++ projects/helloflask-project/helloflask/__init__.py
@@ -0,0 +1,1 @@
+from .helloflask import app
---
+++ projects/helloflask-project/server.py
@@ -0,0 +1,5 @@
+from helloflask import app
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=True)
+    print('TECHIO> open -p 5000')
