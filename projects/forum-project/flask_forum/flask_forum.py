9cd4c27b6d6cc4415a71d0a2e64ef71c8f74a0b6
Zachary Becker <zrbecker@gmail.com>
2017-07-21 22:27:15 -0700

    remove comments

--- projects/forum-project/flask_forum/flask_forum.py
+++ projects/forum-project/flask_forum/flask_forum.py
@@ -10,32 +10,6 @@ app = Flask(__name__)
 DATABASE_FILENAME = 'forum.db'
 DATABASE_PATH = os.path.join(os.path.dirname(__file__), DATABASE_FILENAME)
 
-# class Users:
-#     def __init__(self, db):
-#         self.db = db
-# 
-#     def list_users(self):
-#         return list(self.db.execute('select * from users;'))
-# 
-#     def username(self, user_id):
-#         return self.db.execute('select username from users where user_id = ?',
-#                 str(user_id)).fetchone()[0]
-# 
-# class Threads:
-#     def __init__(self, db):
-#         self.db = db
-# 
-#     def title(self, thread_id):
-#         return self.db.execute('select title from threads where thread_id = ?',
-#                 str(thread_id)).fetchone()[0]
-# 
-#     def list_threads(self):
-#         return list(self.db.execute('select * from threads;'))
-# 
-#     def list_posts(self, thread_id):
-#         return list(self.db.execute(
-#                 'select * from posts where thread_id = ?', str(thread_id)))
-
 @app.route('/')
 def index():
     return threads() 
