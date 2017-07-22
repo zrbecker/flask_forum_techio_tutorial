8c4fe6cc9e8e12ccc42d1b05c7961bdd2b0a6226
Zachary Becker <zrbecker@gmail.com>
2017-07-21 19:55:02 -0700

    fixed test data syntax

--- projects/forum-project/flask_forum/test_data.sql
+++ projects/forum-project/flask_forum/test_data.sql
@@ -1,26 +1,26 @@
-insert into users ('username', 'password') values ('admin', 'password123')
-insert into users ('username', 'password') values ('john', 'smith123')
+insert into users (username, password) values ('admin', 'password123');
+insert into users (username, password) values ('john', 'smith123');
 
-insert into threads ('title') values ('my first thread')
-insert into threads ('title') values ('another thread')
+insert into threads (title) values ('my first thread');
+insert into threads (title) values ('another thread');
 
-insert into posts ('user_id', 'thread_id', 'message')
+insert into posts (user_id, thread_id, message)
   values (
-    (select 'user_id' from users where 'username' = 'admin'),
-    (select 'thread_id' from threads where 'title' = 'my first thread'),
+    (select user_id from users where username = 'admin'),
+    (select thread_id from threads where title = 'my first thread'),
     'First post!'
-  )
+  );
 
-insert into posts ('user_id', 'thread_id', 'message')
+insert into posts (user_id, thread_id, message)
   values (
-    (select 'user_id' from users where 'username' = 'john'),
-    (select 'thread_id' from threads where 'title' = 'my first thread'),
+    (select user_id from users where username = 'john'),
+    (select thread_id from threads where title = 'my first thread'),
     'second post!'
-  )
+  );
 
-insert into posts ('user_id', 'thread_id', 'message')
+insert into posts (user_id, thread_id, message)
   values (
-    (select 'user_id' from users where 'username' = 'john'),
-    (select 'thread_id' from threads where 'title' = 'another thread'),
+    (select user_id from users where username = 'john'),
+    (select thread_id from threads where title = 'another thread'),
     'this is some words!'
-  )
+  );
