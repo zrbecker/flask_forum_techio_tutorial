152ea4712ec9624c6d344028c98a6b0b4fd47d17
Zachary Becker <zrbecker@gmail.com>
2017-07-21 17:53:41 -0700

    debug message

--- projects/helloflask-project/server.py
+++ projects/helloflask-project/server.py
@@ -1,5 +1,8 @@
 from helloflask import app
+import sys
 
 if __name__ == '__main__':
+    print('HELLO 1', file=sys.stderr, flush=True)
     print('TECHIO> open -p 5000 /')
+    print('HELLO 2', file=sys.stderr, flush=True)
     app.run(port=5000)
