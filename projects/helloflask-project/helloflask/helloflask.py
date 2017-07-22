440eed1e8f74384923cb3031b7c0a610d546465d
Zachary Becker <zrbecker@gmail.com>
2017-07-21 18:14:38 -0700

    added a second page

--- projects/helloflask-project/helloflask/helloflask.py
+++ projects/helloflask-project/helloflask/helloflask.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return 'Hello Flask!'
+    return 'Hello Flask! <a href="{}">page</a>'.format(url_for('page'))
 
+@app.route("/page")
+def page():
+    return '<a href="{}">back</a>'.format(url_for('index'))
