ffac962641b2e10908fb762699858226f79ea9d1
Zachary Becker <zrbecker@gmail.com>
2017-07-21 17:56:31 -0700

    trying to make TECHIO> statement work

--- projects/helloflask-project/server.py
+++ projects/helloflask-project/server.py
@@ -3,6 +3,6 @@ import sys
 
 if __name__ == '__main__':
     print('HELLO 1', file=sys.stderr, flush=True)
-    print('TECHIO> open -p 5000 /')
+    print('TECHIO> open --port 8080 /', flush=True)
     print('HELLO 2', file=sys.stderr, flush=True)
     app.run(port=5000)
