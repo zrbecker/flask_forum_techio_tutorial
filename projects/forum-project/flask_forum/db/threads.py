c8186fc9a6e2f0876f6ba67e8004f2701e6224e9
Zachary Becker <zrbecker@gmail.com>
2017-07-21 22:09:38 -0700

    put all database operations in db folder

---
+++ projects/forum-project/flask_forum/db/threads.py
@@ -0,0 +1,52 @@
+SQL_LIST_THREADS = 'select thread_id, title from threads;'
+SQL_LIST_POSTS_FOR_THREAD = 'select post_id, user_id, message from posts where'
+        ' thread_id = ?'
+SQL_LIST_POSTS_FOR_USER = 'select thread_id, user_id, message from posts where'
+        ' post_id = ?'
+
+SQL_READ_THREAD = 'select title from threads where thread_id = ?'
+
+SQL_CREATE_THREAD = 'insert into threads (title) values (?)'
+SQL_CREATE_POST = 'insert into posts (user_id, thread_id, message)'
+        ' values (?, ?, ?)'
+
+class Thread:
+    def __init__(self, thread_id, title):
+        self.thread_id = thread_id
+        self.title = title
+
+class Post:
+    def __init__(self, post_id, user_id, thread_id, message):
+        self.post_id = post_id
+        self.user_id = user_id
+        self.thread_id = thread_id
+        self.message = message
+
+class ThreadDB:
+    def __init__(self, db):
+        self.db = db
+
+    def list_threads(self):
+        return [Thread(thread_id, title)
+                for thread_id, title in self.db.execute(SQL_LIST_THREADS)]
+
+    def list_posts_for_thread(self, thread_id):
+        return [Post(post_id, user_id, thread_id, message)
+                for post_id, user_id, message
+                in self.db.execute(SQL_LIST_POSTS_FOR_THREAD, (thread_id,))]
+
+    def list_posts_for_user(self, user_id):
+        return [Post(post_id, user_id, thread_id, message)
+                for post_id, thread_id, message
+                in self.db.execute(SQL_LIST_POSTS_FOR_USER, (user_id,))]
+
+    def thread(self, thread_id):
+        title, = self.db.execute(SQL_READ_THREAD, (thread_id,))
+        return Thread(thread_id, title)
+
+    def create_thread(self, user_id, title, message):
+        thread_id = self.db.execute(SQL_CREATE_THREAD, (title,)).lastrowid
+        self.create_post(user_id, thread_id, message)
+
+    def create_post(self, user_id, thread_id, message):
+        self.db.execute(SQL_CREATE_POST, (user_id, thread_id, message))
---
+++ projects/forum-project/flask_forum/db/users.py
@@ -0,0 +1,18 @@
+SQL_CREATE_USER = 'insert into users (username, password) values (?, ?)'
+SQL_READ_USERNAME = 'select username from users where user_id = ?'
+SQL_READ_PASSWORD = 'select password from users where user_id = ?'
+
+def UserDB:
+    def __init__(self, db):
+        self.db = db
+
+    def create_user(self, username, password):
+        return self.db.execute(SQL_CREATE_USER, (username, password)).lastrowid
+
+    def read_username(self, user_id):
+        username, = self.db.execute(SQL_READ_USERNAME, (user_id,))
+        return username
+
+    def validate_password(self, user_id, password):
+        db_password, = self.db.execute(SQL_READ_PASSWORD, (user_id,))
+        return password == db_password
